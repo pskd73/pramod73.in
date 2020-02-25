9dde0b562cd7a67f278a43964055f99b98f02910
Pramod Kumar <pramodkumar.damam73@gmail.com>
2020-02-25 23:23:18 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.render("index", {activeNav: 'about'})
+})
+
+app.get("/work", (req, res) => {
+    res.render("work", {activeNav: 'work'})
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
