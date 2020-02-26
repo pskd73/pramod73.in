7fe558d0cba20445b94be31a0baaedab5b13a9d1
Pramod Kumar <pramodkumar.damam73@gmail.com>
2020-02-26 10:27:50 +0530

    Add stats.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
 nohup npm start >> out.log &
-nohup goaccess access.log -o /home/ubuntu/pramod73.in/public/report.html --log-format=COMBINED --real-time-html >> out.log &
---
+++ stats.sh
@@ -0,0 +1,1 @@
+goaccess access.log -o $HOME/pramod73.in/public/report.html --log-format=COMBINED
