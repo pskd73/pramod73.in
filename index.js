0e668855a64b7703740138673c6a80225027ce88
Pramod Kumar <pramodkumar.damam73@gmail.com>
2020-02-26 09:15:31 +0530

    Log to access.log

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const morgan = require("morgan")
+const fs = require("fs")
+const path = require("path")
+
 const app = express()
 
 const port = 3000
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
-app.use(morgan("combined"))
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan("combined", { stream: accessLogStream }))
 
 app.get("/", (req, res) => {
     res.render("index", {activeNav: 'about'})
