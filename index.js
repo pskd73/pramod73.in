024fe8a5b3f0d19a0a0b85cacc41d0a292a03b13
Pramod Kumar <pramodkumar.damam73@gmail.com>
2020-02-26 09:04:51 +0530

    Add logger

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
+
 const port = 3000
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.use(morgan("combined"))
 
 app.get("/", (req, res) => {
     res.render("index", {activeNav: 'about'})
